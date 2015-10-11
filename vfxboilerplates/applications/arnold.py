a5efceba850cc8feef277ef4e390ab79fc13c6ab
Fredrik Brännbacka <fredrik@madcrew.se>
2015-10-11 09:40:31 +0200

    Added id to applications.

--- vfxboilerplates/applications/arnold.py
+++ vfxboilerplates/applications/arnold.py
@@ -2,10 +2,16 @@ import random
 
 
 class Arnold(object):
+    def __init__(self, id=None):
+        self._id = id
+
     @property
     def name(self):
         return 'arnold'
 
     @property
     def maya_id(self):  # TODO: should generate random if id not provided.
-        return "0x%06x" % random.randint(0, 0xFFFFFF)
+        if self._id:
+            return self._id
+        else:
+            return "0x%06x" % random.randint(0, 0xFFFFFF)
